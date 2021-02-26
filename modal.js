71215119c954214838b016a3ab8f06afdfc3418e
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-02-26 08:41:21 +0100

    headline responsive for max 375px, minified css and js

--- modal.js
+++ modal.js
@@ -74,19 +74,14 @@ function getInvalidElements() {
     falseValues.push(lastName);
   }
   // validate email address
-  // validation did not work with const regExEmail
   if (!regExEmail.test(email.value)) {
     falseValues.push(email);
   }
-  // does not return false for ##.##.#####
-  // if (!birthDate.validity.valid) {
-  //   falseValues.push(birthDate);
-  // }
+
   if (!regExDate.test(birthDate.value)) {
     falseValues.push(birthDate);
   }
   // validate tournament attendance
-  // could also do tournamentsAttended.validity.valid ?
   if (isNaN(parseInt(tournamentsAttended.value))) {
     falseValues.push(tournamentsAttended);
   }
@@ -162,84 +157,3 @@ function removeConfirmation() {
 
 // handle submit event
 form.addEventListener("submit", onSubmit);
-
-// Old code
-
-// // check validity of user input fields
-// function getInvalidElements() {
-//   let falseValues = [];
-//   // validate first name
-//   if (firstName.value.length < 2) {
-//     falseValues.push(firstName);
-//   }
-//   // validate last name
-//   if (lastName.value.length < 2) {
-//     falseValues.push(lastName);
-//   }
-//   // validate email address
-//   if (!email.validity.valid || email.value === "") {
-//     falseValues.push(email);
-//   }
-//   if (!birthDate.validity.valid || birthDate.value === "") {
-//     falseValues.push(birthDate);
-//   }
-//   // validate tournament attendance
-//   if (isNaN(parseInt(tournamentsAttended.value))) {
-//     falseValues.push(tournamentsAttended);
-//   }
-//   // validate radio buttons
-//   if (!radioButtonsArray.some((el) => el.checked)) {
-//     falseValues.push(radioButtons[0]);
-//   }
-//   // validate terms and cons
-//   if (!termsAndCons.checked) {
-//     falseValues.push(termsAndCons);
-//   }
-//   return falseValues;
-// }
-
-// // setting invalid event listener on form element
-// form.addEventListener("invalid", errorStyling);
-
-// // function to set error styling
-// function errorStyling(e) {
-//   let targetID = e.target.getAttribute("id");
-//   let targetParent = e.target.parentElement;
-//   targetParent.setAttribute("data-error-visible", "true");
-//   targetParent.setAttribute("data-error", errorMessages[targetID]);
-// }
-
-// // validate user input on submit
-// form.addEventListener("submit", (e) => {
-//   // prevent submission and page refresh
-//   e.preventDefault();
-//   // remove styling and messages of former invalid input fields
-//   removeInvalidStyling();
-//   // check validity of user input
-//   let invalidElements = getInvalidElements();
-//   if (invalidElements.length === 0) {
-//     // SOME FUNCTION FOR AJAX request
-//     console.log("submitted!");
-//     // change modal to confirmation message
-//     setConfirmation();
-//   } else {
-//     // set style and error messages for invalid input
-//     styleInvalidElements(invalidElements);
-//   }
-// });
-
-// // validate user input on submit
-// function validate() {
-//   // remove styling and messages of former invalid input fields
-//   removeInvalidStyling();
-//   // check validity of user input
-//   let invalidElements = getInvalidElements();
-//   if (invalidElements.length === 0) {
-//     //return valid.length === 0;
-//     return true;
-//   } else {
-//     // set style and error messages for invalid input
-//     setInvalidStyling(invalidElements);
-//     return false;
-//   }
-// }
